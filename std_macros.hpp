18dc842906ddf5af661d954359892c2fb52f824c
Michal Walenciak <Kicer86@gmail.com>
2011-11-19 19:48:24 +0100

    macros: wyrzucenie książkowych makr, dodanie makra foreach

--- std_macros.hpp
+++ std_macros.hpp
@@ -1,23 +1,6 @@
 //some usefull std macros
 
-// W programach bardzo rzadko mozna znalezc w pelni zapisana instrukcje petli. Zamiast niej, wykorzystywane sa trzy nastepujace makra:
-// FOR - petla zwiekszajaca zmienna x od b do e wlacznie
-#define FOR(x, b, e) for(int x = b; x <= (e); ++x)
-// FORD - petla zmniejszajaca zmienna x od b do e wlacznie
-#define FORD(x, b, e) for(int x = b; x >= (e); --x)
-// REP - petla zwiekszajaca zmienna x od 0 do n. Jest ona bardzo czesto wykorzystywana do konstruowania i przegladania struktur danych
-#define REP(x, n) for(int x = 0; x < (n); ++x)
-// Makro VAR(v,n) deklaruje nowa zmienna o nazwie v oraz typie i wartosci zmiennej n. Jest ono czesto wykorzystywane podczas operowania na iteratorach struktur danych z biblioteki STL, ktorych nazwy typow sa bardzo dlugie
-#define VAR(v, n) __typeof(n) v = (n)
-// ALL(c) reprezentuje pare iteratorow wskazujacych odpowiednio na pierwszy i za ostatni element w strukturach danych STL. Makro to jest bardzo przydatne chociazby w przypadku korzystania z funkcji sort, ktora jako parametry przyjmuje pare iteratorow reprezentujacych przedzial elementow do posortowania.
-#define ALL(c) (c).begin(), (c).end()
-// Ponizsze makro sluzy do wyznaczania rozmiaru struktur danych STL. Uzywa sie go w programach, zamiast pisac po prostu x.size() z uwagi na fakt, iz wyrazenie x.size() jest typu unsigned int i w przypadku porownywania z typem int, w procesie kompilacji generowane jest ostrzezenie.
-#define SIZE(x) ((int)(x).size())
-// Bardzo pozyteczne makro, sluzace do iterowania po wszystkich elementach w strukturach danych STL.
-#define FOREACH(i, c) for(VAR(i, (c).begin()); i != (c).end(); ++i)
-// Skrot - zamiast pisac push_back podczas wstawiania elementow na koniec struktury danych, takiej jak vector, wystarczy napisac PB
-#define PB push_back
-// Podobnie - zamiast first bedziemy pisali po prostu ST
-#define ST first
-// a zamiast second - ND.
-#define ND second
+//macro for iterating over std's containers
+#define FOREACH(element, set) \
+    for(auto it = (set).begin(); it != (set).end();)   \
+        for (auto element = *it; it != (set).end(); ++it)
