3c140309f544beec0f58c0ab4c226fb0d8478e60
Michał Walenciak <Kicer86@gmail.com>
2011-12-25 14:16:48 +0100

    std_macros: some usefull macro for calling pointer to object's members

--- std_macros.hpp
+++ std_macros.hpp
@@ -4,3 +4,5 @@
 #define FOREACH(element, set) \
     for(auto it = (set).begin(); it != (set).end();)   \
         for (auto element = *it; it != (set).end(); ++it)
+
+#define CALL_MEMBER_FN(object, ptrToMember)  ((object).*(ptrToMember))
