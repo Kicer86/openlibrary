b963c0761a4c3d456418625be8339f1f8392c500
Michal Walenciak <Kicer86@gmail.com>
2012-08-14 21:22:32 +0200

    base class for A* algorithm added

---
+++ router/A_star/router.hpp
@@ -0,0 +1,22 @@
+
+/***************************************
+ *    Base for A* routing algorithm    *
+ *      Author: Micha³ Walenciak       *
+ *      Creation date: 14.08.2012      *
+ ***************************************/
+
+#ifndef OPENLIBRARY_A_START_ROUTER_HPP
+#define OPENLIBRARY_A_START_ROUTER_HPP
+
+namespace OpenLibrary
+{
+    namespace Router
+    {
+        class AStar
+        {
+
+        };
+    }
+}
+
+#endif
