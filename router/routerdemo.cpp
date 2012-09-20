1aefbc826102268abd38a20db2c99c9d875f0e82
Michał Walenciak <Kicer86@gmail.com>
2012-09-20 22:20:05 +0200

    router: old files removed; preparations for usage of new function for registering libraries

--- router/routerdemo.cpp
+++ router/routerdemo.cpp
@@ -21,7 +21,7 @@
 #include <QPainter>
 
 #include "routerdemo.hpp"
-#include "router.hpp"
+
 
 #include "A_star/std_alg.hpp"
 
@@ -52,98 +52,3 @@ namespace
     } _run;
 }
 
-
-#define WIDTH 30
-#define HEIGHT 10
-#define SIZE 20
-
-void RoutingThread::run()
-{
-  router=new Router(Router::Area(Router::Point(0,0), Router::Point(WIDTH,HEIGHT)));
-  router->addObstacle(Router::Area(Router::Point(10,0), Router::Point(12,8)));
-  router->addObstacle(Router::Area(Router::Point(20,9), Router::Point(22,2)));
-  router->route(Router::Point(0,0), Router::Point(25,9));
-
-  exit();
-}
-
-
-RoutingThread::~RoutingThread()
-{
-  delete router;
-}
-
-
-char** RoutingThread::getField()
-{
-  return router->getField();
-}
-
-
-RouterDemo::RouterDemo(QWidget* parent, Qt::WindowFlags f): QWidget(parent, f)
-{
-  thread.start();
-
-  connect(&timer, SIGNAL(timeout()), this, SLOT(update()));
-  timer.start(200);    //repaint frequency
-}
-
-
-void RouterDemo::paintEvent(QPaintEvent* e)
-{
-  char **field=thread.getField();
-
-  QPainter painter(this);
-
-  for (int y=0; y< HEIGHT; y++)
-    for (int x=0; x< WIDTH; x++)
-    {
-      switch (field[y][x])
-      {
-        case 0:
-          painter.setBrush(QColor(Qt::red));
-          break;
-
-        case 1:
-          painter.setBrush(QColor(Qt::darkRed));
-          break;
-
-        case 2:
-          painter.setBrush(QColor(Qt::blue));
-          break;
-
-        case 3:
-          painter.setBrush(QColor(Qt::darkBlue));
-          break;
-
-        case 4:
-          painter.setBrush(QColor(Qt::green));
-          break;
-
-        case 5:
-          painter.setBrush(QColor(Qt::darkGreen));
-          break;
-
-        case 6:
-          painter.setBrush(QColor(Qt::cyan));
-          break;
-
-        case 7:
-          painter.setBrush(QColor(Qt::darkCyan));
-          break;
-
-        case 15:
-          painter.setBrush(QColor(Qt::black));
-          break;
-
-        default:
-          painter.setBrush(QColor(Qt::white));
-          break;
-      }
-
-      QRect rect(x*SIZE, y*SIZE, SIZE, SIZE);
-      painter.drawRect(rect);
-    }
-
-  e->accept();
-}
