228cf74d821d44afb3b0bcaac2bd12c15a09968b
Michał Walenciak <Kicer86@gmail.com>
2012-10-20 22:23:28 +0200

    tests added to watching cmake rules about generating cmake variables for sublibraries

--- scripts/cmake_tests.cmake
+++ scripts/cmake_tests.cmake
@@ -1,1 +1,8 @@
- 
+
+function(runTests)
+    if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+        message("Running cmake rules tests")
+        execute_process(COMMAND sh ./doTests.sh
+                        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/scripts)
+    endif(${CMAKE_BUILD_TYPE} EQUAL "Debug")
+endfunction(runTests)
