8e0f7c9561b8d8d06ca290016dd7b551bf633eed
Michał Walenciak <Kicer86@gmail.com>
2012-10-20 22:36:22 +0200

    refactor

--- cmake/cmake_tests.cmake
+++ cmake/cmake_tests.cmake
@@ -3,6 +3,6 @@ function(runTests)
     if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
         message("Running cmake rules tests")
         execute_process(COMMAND sh ./doTests.sh
-                        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/scripts)
-    endif(${CMAKE_BUILD_TYPE} EQUAL "Debug")
+                        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/cmake)
+    endif(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
 endfunction(runTests)
