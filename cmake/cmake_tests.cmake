eec1ee57938f1d42f0d9960eb25552d61713f2b0
Michał Walenciak <Kicer86@gmail.com>
2012-10-21 17:01:33 +0200

    math: cpputest tests introduced

--- cmake/cmake_tests.cmake
+++ cmake/cmake_tests.cmake
@@ -4,5 +4,7 @@ function(runTests)
         add_custom_target(cmake_tests
                           COMMAND sh ./doTests.sh ${LIBRARIES_CMAKE}
                           WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/cmake)
+
+        add_dependencies(test cmake_tests)
     endif(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
 endfunction(runTests)
