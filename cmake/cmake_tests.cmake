bf546d0cf3e1c192c87d26f0bd1e6875c1548405
unknown <Michal@virtualbox-winx.(none)>
2012-11-04 15:26:40 +0100

    cmake: fixing warning on windows
    cmake_build_type doesn't exist here - CMAKE_CONFIGURATION_TYPES:STRING is used insted

--- cmake/cmake_tests.cmake
+++ cmake/cmake_tests.cmake
@@ -1,10 +1,10 @@
 
 function(runTests)
-    if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+    if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
         add_custom_target(performCMakeRulesChecks
                           COMMAND sh ./doTests.sh ${LIBRARIES_CMAKE}
                           WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/cmake)
 
         add_dependencies(test performCMakeRulesChecks)
-    endif(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+    endif("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
 endfunction(runTests)
