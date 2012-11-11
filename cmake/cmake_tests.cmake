772c87ef1bb5202cc1443c4ad31b7e1a94a11567
Michał Walenciak <Kicer86@gmail.com>
2012-11-11 15:16:23 +0100

    cmake: turning some unix related commands to be run only on unix

--- cmake/cmake_tests.cmake
+++ cmake/cmake_tests.cmake
@@ -1,10 +1,13 @@
 
 function(runTests)
-    if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+
+    #as some unix commands are being used here - do not add anything on non unix systems
+    if(UNIX)
         add_custom_target(performCMakeRulesChecks
                           COMMAND sh ./doTests.sh ${LIBRARIES_CMAKE}
                           WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/cmake)
 
         add_dependencies(test performCMakeRulesChecks)
-    endif("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+    endif(UNIX)
+    
 endfunction(runTests)
