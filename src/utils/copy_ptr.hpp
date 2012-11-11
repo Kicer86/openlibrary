88fffd8148f69a54fc4c3e91fae7b1310ea3e802
Michał Walenciak <Kicer86@gmail.com>
2012-11-11 14:49:48 +0100

    utils: major fixes for copy_ptr - reorganization
    previous implementation was just broken

--- src/utils/copy_ptr.hpp
+++ src/utils/copy_ptr.hpp
@@ -11,35 +11,50 @@ namespace
     {
         delete ptr;
     }
+    
+    template<class T>
+    void copier(T* &to_ptr, const T *from_ptr)
+    {
+        *to_ptr = *from_ptr;
+    }
 }
 
 
 //copy pointer
+//copy_ptr can be used for storing class' private data which on class copy should also be copied
+//operator= of copy_ptr and copy consturctor of copy_ptr are calling operator= on encapsulated pointer
 template<class T>
 class copy_ptr
 {
     public:
         typedef void (*Deleter)(T *);
+        typedef void (*Copier)(T* &, const T *);
         
-        copy_ptr(T *v = nullptr, Deleter d = &deleter): m_ptr(v), m_deleter(d)
+        copy_ptr(T *ptr = nullptr, Deleter d = &deleter, Copier c = &copier): m_ptr(ptr), m_deleter(d), m_copier(c)
         {
 
         }
-
-        copy_ptr(const T &other) 
+        
+        copy_ptr(const copy_ptr<T> &other)
         {
-            delete m_ptr;
-            m_ptr = new T(other);
-        }
+            copy(other);
+        }        
 
         virtual ~copy_ptr()
         {
-            getDeleter()(m_ptr);
+            del();
         }
 
-        copy_ptr& operator=(const T &other)
+        copy_ptr& operator=(T *ptr)
         {
-            *m_ptr = *other;
+            del();
+            m_ptr = ptr;
+        }
+        
+        copy_ptr& operator=(const copy_ptr<T> &other)
+        {
+            del();
+            copy(other);
         }
 
         T* operator->()
@@ -62,14 +77,42 @@ class copy_ptr
             return *m_ptr;
         }
 
-    private:
+    private:        
         T *m_ptr;
         Deleter m_deleter;
-        
+        Copier m_copier;
+                
         Deleter getDeleter() const
         {
             return m_deleter;
         }
+        
+        Copier getCopier() const
+        {
+            return m_copier;
+        }
+        
+        //copy another copy_ptr
+        void copy(const copy_ptr<T> &other)
+        {
+            copy(other.m_ptr);
+            m_deleter = other.m_deleter;
+            m_copier = other.m_copier;
+        }
+        
+        //copy object
+        void copy(T *ptr)
+        {
+            getCopier()(m_ptr, ptr);            
+        }
+        
+        //delete object
+        void del() const
+        {
+            getDeleter()(m_ptr);
+        }
+                
+
 };
 
 #endif
