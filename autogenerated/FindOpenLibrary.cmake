a7b7f38364a9e138c2f95105803c026aff744cd5
Michał Walenciak <Kicer86@gmail.com>
2012-11-19 21:47:32 +0100

    autogenerated regenerated

--- autogenerated/FindOpenLibrary.cmake
+++ autogenerated/FindOpenLibrary.cmake
@@ -2,6 +2,7 @@
 #rules for finding OpenLibrary
 #based on http://www.vtk.org/Wiki/CMake:How_To_Find_Libraries
 #and on FindQt4.cmake for Mageia Linux (components part)
+#http://cmake.org/gitweb?p=cmake.git;a=blob;f=Modules/readme.txt
 
 include(FindPackageHandleStandardArgs)
 
@@ -11,7 +12,8 @@ if(OpenLibrary_FIND_COMPONENTS)
         STRING(TOUPPER ${component} comp_name)
         
         #try to load component's data
-        set(OL_ENABLE_COMPONENT_${TOUPPER} 1)
+        set(OL_ENABLE_COMPONENT_${comp_name} 1)
+        message("Finding component ${comp_name}")
         
     endforeach(component ${OpenLibrary_FIND_COMPONENTS})
 
@@ -22,7 +24,71 @@ endif(OpenLibrary_FIND_COMPONENTS)
 find_path(OPENLIBRARY_INCLUDE_DIRS OpenLibrary/common/debug.hpp)
 
 find_package_handle_standard_args(OpenLibrary DEFAULT_MSG OPENLIBRARY_INCLUDE_DIRS)
-set ( HTMLPARSER_LIBRARIES htmlParser ) 
-set ( MATH_LIBRARIES math ) 
-set ( QTEXT_LIBRARIES QtExt ) 
-set ( UTILS_LIBRARIES utils ) 
+
+
+if(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_HTMLPARSER)
+
+    find_path(OPEN_LIBRARY_HTMLPARSER_INCLUDE_DIRS NAMES searchlist.hpp htmltaglist.hpp htmltag.hpp htmlparser.hpp
+              HINTS ${OPENLIBRARY_INCLUDE_DIRS})
+    
+    find_library(OPEN_LIBRARY_HTMLPARSER_LIBRARIES NAMES htmlParser
+                 HINTS ${OPENLIBRARY_INCLUDE_DIRS}/.. )
+    
+    find_package_handle_standard_args(OpenLibrary_HTMLPARSER DEFAULT_MSG
+                                      OPEN_LIBRARY_HTMLPARSER_INCLUDE_DIRS OPEN_LIBRARY_HTMLPARSER_LIBRARIES)
+
+    mark_as_advanced(OPEN_LIBRARY_HTMLPARSER_INCLUDE_DIRS OPEN_LIBRARY_HTMLPARSER_LIBRARIES)
+
+endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_HTMLPARSER)
+
+
+
+if(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_MATH)
+
+    find_path(OPEN_LIBRARY_MATH_INCLUDE_DIRS NAMES crc32.hpp math.hpp
+              HINTS ${OPENLIBRARY_INCLUDE_DIRS})
+    
+    find_library(OPEN_LIBRARY_MATH_LIBRARIES NAMES math
+                 HINTS ${OPENLIBRARY_INCLUDE_DIRS}/.. )
+    
+    find_package_handle_standard_args(OpenLibrary_MATH DEFAULT_MSG
+                                      OPEN_LIBRARY_MATH_INCLUDE_DIRS OPEN_LIBRARY_MATH_LIBRARIES)
+
+    mark_as_advanced(OPEN_LIBRARY_MATH_INCLUDE_DIRS OPEN_LIBRARY_MATH_LIBRARIES)
+
+endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_MATH)
+
+
+
+if(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_QTEXT)
+
+    find_path(OPEN_LIBRARY_QTEXT_INCLUDE_DIRS NAMES qtext_choosefile.hpp qtext_progressbar.hpp
+              HINTS ${OPENLIBRARY_INCLUDE_DIRS})
+    
+    find_library(OPEN_LIBRARY_QTEXT_LIBRARIES NAMES QtExt
+                 HINTS ${OPENLIBRARY_INCLUDE_DIRS}/.. )
+    
+    find_package_handle_standard_args(OpenLibrary_QTEXT DEFAULT_MSG
+                                      OPEN_LIBRARY_QTEXT_INCLUDE_DIRS OPEN_LIBRARY_QTEXT_LIBRARIES)
+
+    mark_as_advanced(OPEN_LIBRARY_QTEXT_INCLUDE_DIRS OPEN_LIBRARY_QTEXT_LIBRARIES)
+
+endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_QTEXT)
+
+
+
+if(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_UTILS)
+
+    find_path(OPEN_LIBRARY_UTILS_INCLUDE_DIRS NAMES copy_ptr.hpp cpuinfo.hpp
+              HINTS ${OPENLIBRARY_INCLUDE_DIRS})
+    
+    find_library(OPEN_LIBRARY_UTILS_LIBRARIES NAMES utils
+                 HINTS ${OPENLIBRARY_INCLUDE_DIRS}/.. )
+    
+    find_package_handle_standard_args(OpenLibrary_UTILS DEFAULT_MSG
+                                      OPEN_LIBRARY_UTILS_INCLUDE_DIRS OPEN_LIBRARY_UTILS_LIBRARIES)
+
+    mark_as_advanced(OPEN_LIBRARY_UTILS_INCLUDE_DIRS OPEN_LIBRARY_UTILS_LIBRARIES)
+
+endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_UTILS)
+
