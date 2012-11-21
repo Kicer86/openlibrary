b6c5d0ca1fd9c3fcc92e85c47092b2943770607f
Michał Walenciak <Kicer86@gmail.com>
2012-11-21 21:00:10 +0100

    regerneration of autogenerated files

--- autogenerated/FindOpenLibrary.cmake
+++ autogenerated/FindOpenLibrary.cmake
@@ -22,22 +22,25 @@ endif(OpenLibrary_FIND_COMPONENTS)
 
 #find common stuff
 find_path(OPENLIBRARY_INCLUDE_DIRS OpenLibrary/common/debug.hpp)
+find_library(OPENLIBRARY_LIBRARIES OpenLibrary/libcommon.so)
 
 find_package_handle_standard_args(OpenLibrary DEFAULT_MSG OPENLIBRARY_INCLUDE_DIRS)
 
 
 if(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_HTMLPARSER)
 
-    find_path(OPEN_LIBRARY_HTMLPARSER_INCLUDE_DIRS NAMES searchlist.hpp htmltaglist.hpp htmltag.hpp htmlparser.hpp
+    get_filename_component(OL_LIB_PATH ${OPENLIBRARY_LIBRARIES} PATH)
+
+    find_path(OPENLIBRARY_HTMLPARSER_INCLUDE_DIRS NAMES OpenLibrary/htmlParser/htmltag.hpp OpenLibrary/htmlParser/htmltaglist.hpp OpenLibrary/htmlParser/searchlist.hpp OpenLibrary/htmlParser/libhtmlParser.so
               HINTS ${OPENLIBRARY_INCLUDE_DIRS})
     
-    find_library(OPEN_LIBRARY_HTMLPARSER_LIBRARIES NAMES htmlParser
-                 HINTS ${OPENLIBRARY_INCLUDE_DIRS}/.. )
+    find_library(OPENLIBRARY_HTMLPARSER_LIBRARIES NAMES htmlParser
+                 HINTS ${OL_LIB_PATH} )
     
     find_package_handle_standard_args(OpenLibrary_HTMLPARSER DEFAULT_MSG
-                                      OPEN_LIBRARY_HTMLPARSER_INCLUDE_DIRS OPEN_LIBRARY_HTMLPARSER_LIBRARIES)
+                                      OPENLIBRARY_HTMLPARSER_INCLUDE_DIRS OPENLIBRARY_HTMLPARSER_LIBRARIES)
 
-    mark_as_advanced(OPEN_LIBRARY_HTMLPARSER_INCLUDE_DIRS OPEN_LIBRARY_HTMLPARSER_LIBRARIES)
+    mark_as_advanced(OPENLIBRARY_HTMLPARSER_INCLUDE_DIRS OPENLIBRARY_HTMLPARSER_LIBRARIES)
 
 endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_HTMLPARSER)
 
@@ -45,16 +48,18 @@ endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_HTMLPARSER)
 
 if(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_MATH)
 
-    find_path(OPEN_LIBRARY_MATH_INCLUDE_DIRS NAMES crc32.hpp math.hpp
+    get_filename_component(OL_LIB_PATH ${OPENLIBRARY_LIBRARIES} PATH)
+
+    find_path(OPENLIBRARY_MATH_INCLUDE_DIRS NAMES OpenLibrary/math/crc32.hpp OpenLibrary/math/libmath.so
               HINTS ${OPENLIBRARY_INCLUDE_DIRS})
     
-    find_library(OPEN_LIBRARY_MATH_LIBRARIES NAMES math
-                 HINTS ${OPENLIBRARY_INCLUDE_DIRS}/.. )
+    find_library(OPENLIBRARY_MATH_LIBRARIES NAMES math
+                 HINTS ${OL_LIB_PATH} )
     
     find_package_handle_standard_args(OpenLibrary_MATH DEFAULT_MSG
-                                      OPEN_LIBRARY_MATH_INCLUDE_DIRS OPEN_LIBRARY_MATH_LIBRARIES)
+                                      OPENLIBRARY_MATH_INCLUDE_DIRS OPENLIBRARY_MATH_LIBRARIES)
 
-    mark_as_advanced(OPEN_LIBRARY_MATH_INCLUDE_DIRS OPEN_LIBRARY_MATH_LIBRARIES)
+    mark_as_advanced(OPENLIBRARY_MATH_INCLUDE_DIRS OPENLIBRARY_MATH_LIBRARIES)
 
 endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_MATH)
 
@@ -62,16 +67,18 @@ endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_MATH)
 
 if(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_QTEXT)
 
-    find_path(OPEN_LIBRARY_QTEXT_INCLUDE_DIRS NAMES qtext_choosefile.hpp qtext_progressbar.hpp
+    get_filename_component(OL_LIB_PATH ${OPENLIBRARY_LIBRARIES} PATH)
+
+    find_path(OPENLIBRARY_QTEXT_INCLUDE_DIRS NAMES OpenLibrary/QtExt/qtext_choosefile.hpp OpenLibrary/QtExt/libQtExt.so
               HINTS ${OPENLIBRARY_INCLUDE_DIRS})
     
-    find_library(OPEN_LIBRARY_QTEXT_LIBRARIES NAMES QtExt
-                 HINTS ${OPENLIBRARY_INCLUDE_DIRS}/.. )
+    find_library(OPENLIBRARY_QTEXT_LIBRARIES NAMES QtExt
+                 HINTS ${OL_LIB_PATH} )
     
     find_package_handle_standard_args(OpenLibrary_QTEXT DEFAULT_MSG
-                                      OPEN_LIBRARY_QTEXT_INCLUDE_DIRS OPEN_LIBRARY_QTEXT_LIBRARIES)
+                                      OPENLIBRARY_QTEXT_INCLUDE_DIRS OPENLIBRARY_QTEXT_LIBRARIES)
 
-    mark_as_advanced(OPEN_LIBRARY_QTEXT_INCLUDE_DIRS OPEN_LIBRARY_QTEXT_LIBRARIES)
+    mark_as_advanced(OPENLIBRARY_QTEXT_INCLUDE_DIRS OPENLIBRARY_QTEXT_LIBRARIES)
 
 endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_QTEXT)
 
@@ -79,16 +86,18 @@ endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_QTEXT)
 
 if(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_UTILS)
 
-    find_path(OPEN_LIBRARY_UTILS_INCLUDE_DIRS NAMES copy_ptr.hpp cpuinfo.hpp
+    get_filename_component(OL_LIB_PATH ${OPENLIBRARY_LIBRARIES} PATH)
+
+    find_path(OPENLIBRARY_UTILS_INCLUDE_DIRS NAMES OpenLibrary/utils/copy_ptr.hpp OpenLibrary/utils/libutils.so
               HINTS ${OPENLIBRARY_INCLUDE_DIRS})
     
-    find_library(OPEN_LIBRARY_UTILS_LIBRARIES NAMES utils
-                 HINTS ${OPENLIBRARY_INCLUDE_DIRS}/.. )
+    find_library(OPENLIBRARY_UTILS_LIBRARIES NAMES utils
+                 HINTS ${OL_LIB_PATH} )
     
     find_package_handle_standard_args(OpenLibrary_UTILS DEFAULT_MSG
-                                      OPEN_LIBRARY_UTILS_INCLUDE_DIRS OPEN_LIBRARY_UTILS_LIBRARIES)
+                                      OPENLIBRARY_UTILS_INCLUDE_DIRS OPENLIBRARY_UTILS_LIBRARIES)
 
-    mark_as_advanced(OPEN_LIBRARY_UTILS_INCLUDE_DIRS OPEN_LIBRARY_UTILS_LIBRARIES)
+    mark_as_advanced(OPENLIBRARY_UTILS_INCLUDE_DIRS OPENLIBRARY_UTILS_LIBRARIES)
 
 endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_UTILS)
 
