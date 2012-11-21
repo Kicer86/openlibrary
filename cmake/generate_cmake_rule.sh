55656ea57465ae0023ac133794ebccae2a10fa8f
Michał Walenciak <Kicer86@gmail.com>
2012-11-21 20:59:56 +0100

    generate_cmake_rule.sh: using common.so as a base for other modules

--- cmake/generate_cmake_rule.sh
+++ cmake/generate_cmake_rule.sh
@@ -11,16 +11,18 @@ echo "
 
 if(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_$project_name)
 
-    find_path(OPEN_LIBRARY_${project_name}_INCLUDE_DIRS NAMES $header
+    get_filename_component(OL_LIB_PATH \${OPENLIBRARY_LIBRARIES} PATH)
+
+    find_path(OPENLIBRARY_${project_name}_INCLUDE_DIRS NAMES $header
               HINTS \${OPENLIBRARY_INCLUDE_DIRS})
     
-    find_library(OPEN_LIBRARY_${project_name}_LIBRARIES NAMES $lib
-                 HINTS \${OPENLIBRARY_INCLUDE_DIRS}/.. )
+    find_library(OPENLIBRARY_${project_name}_LIBRARIES NAMES $lib
+                 HINTS \${OL_LIB_PATH} )
     
     find_package_handle_standard_args(OpenLibrary_${project_name} DEFAULT_MSG
-                                      OPEN_LIBRARY_${project_name}_INCLUDE_DIRS OPEN_LIBRARY_${project_name}_LIBRARIES)
+                                      OPENLIBRARY_${project_name}_INCLUDE_DIRS OPENLIBRARY_${project_name}_LIBRARIES)
 
-    mark_as_advanced(OPEN_LIBRARY_${project_name}_INCLUDE_DIRS OPEN_LIBRARY_${project_name}_LIBRARIES)
+    mark_as_advanced(OPENLIBRARY_${project_name}_INCLUDE_DIRS OPENLIBRARY_${project_name}_LIBRARIES)
 
 endif(OPENLIBRARY_FOUND AND OL_ENABLE_COMPONENT_$project_name)
 "
