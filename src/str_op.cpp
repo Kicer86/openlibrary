2d0aee9a7c8d4d3eac75383f7b92cc77d0d97858
Michał Walenciak <Kicer86@gmail.com>
2012-12-18 22:15:49 +0100

    todo

--- src/str_op.cpp
+++ src/str_op.cpp
@@ -6,42 +6,44 @@ namespace Strings
 {
     std::string *stripQuotas(std::string *s)
     {
-      std::string &str=*s;         //str and s are now the same
-      if (str.empty()==false &&
-          str[0]==str[str.length()-1] &&
-          (str[0]=='"' || str[0]=='\'')
-         )
-      {
-        str.erase(0, 1);               //remove 1st " or '
-        str.erase(str.length()-1, 1);  //remove last one
-      }
-      return &str;
+        std::string &str=*s;         //str and s are now the same
+        
+        if (str.empty()==false &&
+            str[0]==str[str.length()-1] &&
+            (str[0]=='"' || str[0]=='\'')
+            )
+        {
+            str.erase(0, 1);               //remove 1st " or '
+            str.erase(str.length()-1, 1);  //remove last one
+        }
+        
+        return &str;
     }
 
     std::string stripBlanks(const std::string &s)
     {
-      std::string str=s;
+        std::string str=s;
 
-      //from the begining
-      int len=str.length();
-      int l=0;
+        //from the begining
+        int len=str.length();
+        int l=0;
 
-      while (l<len && str[l]<=32)
-        l++;
+        while (l<len && str[l]<=32)
+            l++;
 
-      if (l)
-        str.erase(0, l);
+        if (l)
+            str.erase(0, l);
 
-      //from the end
-      len=str.length();
-      int p=len-1;
-      while (p>=0 && str[p]<=32)
-        p--;
+        //from the end
+        len=str.length();
+        int p=len-1;
+        while (p>=0 && str[p]<=32)
+            p--;
 
-      p++;
-      if (p<len)
-        str.erase(p);  //remove them all from the end
+        p++;
+        if (p<len)
+            str.erase(p);  //remove them all from the end
 
-      return str;
+        return str;
     }
 }
