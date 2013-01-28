7f9017cdc2445458e08f148aad218688387bb67f
Michał Walenciak <Kicer86@gmail.com>
2013-01-28 21:25:23 +0100

    utils: first test for data_ptr

--- src/utils/tests/utils_tests.cpp
+++ src/utils/tests/utils_tests.cpp
@@ -1,14 +1,33 @@
 
 #include <CppUTest/TestHarness.h>
 
-#include "../copy_ptr.hpp"
+#include "../data_ptr.hpp"
 
-TEST_GROUP(UtilsTest)
+TEST_GROUP(DataPtrTest)
 {
 };
 
-TEST(UtilsTest, FirstTest)
+TEST(DataPtrTest, ShouldTakeOwnerShipOverPointerPassed)
 {
+    struct Test
+    {
+        Test(int *v): m_value(v)
+        {
+            *m_value = 0;
+        }
 
+        ~Test()
+        {
+            *m_value = 1;
+        }
+
+        int *m_value;
+    };
+
+    int m = 0;
+    Test *t = new Test(&m);
+    data_ptr<Test> *ptr = new data_ptr<Test>(t);
+    delete ptr;
+
+    CHECK_EQUAL(1, m);
 }
-
