07498c30df7b3be30d28b039fdd05dff0e2d561e
Michał Walenciak <Kicer86@gmail.com>
2013-01-28 22:16:11 +0100

    utils: many tests for data_ptr added. data_ptr bug fixes

--- src/utils/data_ptr.hpp
+++ src/utils/data_ptr.hpp
@@ -15,14 +15,14 @@ namespace
     template<class T>
     void copier(T* &to_ptr, const T *from_ptr)
     {
-        *to_ptr = *from_ptr;
+        to_ptr = new T(*from_ptr);
     }
 }
 
 
 //data pointer
 //data_ptr can be used for storing class' private data which on class copy should also be copied
-//operator= of data_ptr and copy consturctor of data_ptr are calling operator= on encapsulated pointer
+//operator= of data_ptr and copy constructor of data_ptr are calling copy constructor on encapsulated pointer
 template<class T>
 class data_ptr
 {
@@ -35,7 +35,7 @@ class data_ptr
 
         }
 
-        data_ptr(const data_ptr<T> &other)
+        data_ptr(const data_ptr<T> &other): m_ptr(0)
         {
             copy(other);
         }
@@ -45,16 +45,20 @@ class data_ptr
             del();
         }
 
+        //takes ownership over data
         data_ptr& operator=(T *ptr)
         {
             del();
             m_ptr = ptr;
+
+            return *this;
         }
 
         data_ptr& operator=(const data_ptr<T> &other)
         {
-            del();
             copy(other);
+
+            return *this;
         }
 
         T* operator->()
@@ -96,17 +100,21 @@ class data_ptr
         //copy another copy_ptr
         void copy(const data_ptr<T> &other)
         {
-            //copy data
-            copy(other.m_ptr);
-
             //copy operators
             m_deleter = other.m_deleter;
             m_copier = other.m_copier;
+
+            //copy data
+            copy(other.m_ptr);
         }
 
         //copy object
         void copy(T *ptr)
         {
+            //delete old data
+            del();
+
+            //create copy
             getCopier()(m_ptr, ptr);
         }
 
--- src/utils/tests/utils_tests.cpp
+++ src/utils/tests/utils_tests.cpp
@@ -11,23 +11,148 @@ TEST(DataPtrTest, ShouldTakeOwnerShipOverPointerPassed)
 {
     struct Test
     {
-        Test(int *v): m_value(v)
-        {
-            *m_value = 0;
-        }
-
-        ~Test()
-        {
-            *m_value = 1;
-        }
-
-        int *m_value;
+        int m_dat;
     };
 
-    int m = 0;
-    Test *t = new Test(&m);
-    data_ptr<Test> *ptr = new data_ptr<Test>(t);
-    delete ptr;
+    Test *t = new Test;
+    data_ptr<Test> ptr(t);
 
-    CHECK_EQUAL(1, m);
+    //CppUTest will automaticaly check for memory leak
+}
+
+
+TEST(DataPtrTest, ShouldCopyDataWhenDataPtrCopied)
+{
+    struct Test
+    {
+        Test(): m_data(1) {}
+
+        int m_data;
+    };
+
+    Test *test = new Test;
+
+    data_ptr<Test> ptr1(test);
+    data_ptr<Test> ptr2 = ptr1;
+
+    ptr1->m_data = 5;
+    ptr2->m_data = 7;
+
+    CHECK_EQUAL(5, ptr1->m_data);
+    CHECK_EQUAL(7, ptr2->m_data);
+}
+
+
+TEST(DataPtrTest, ShouldCopyDataWhenDataPtrAssigned)
+{
+    struct Test
+    {
+        Test(): m_data(1) {}
+
+        int m_data;
+    };
+
+    Test *test = new Test;
+
+    data_ptr<Test> ptr1(test);
+    data_ptr<Test> ptr2;
+
+    ptr2 = ptr1;
+
+    ptr1->m_data = 3;
+    ptr2->m_data = 8;
+
+    CHECK_EQUAL(3, ptr1->m_data);
+    CHECK_EQUAL(8, ptr2->m_data);
+}
+
+
+TEST(DataPtrTest, ShouldCopyDataWhenDataAssigned)
+{
+    struct Test
+    {
+        Test(): m_data(1) {}
+
+        int m_data;
+    };
+
+    Test *test = new Test;
+    test->m_data = 1;
+
+    data_ptr<Test> ptr1;
+    ptr1 = test;
+    test->m_data = 4;
+    ptr1->m_data = 6;
+
+    //data_ptr takes ownership ower pointer, so both values should be equal
+    CHECK_EQUAL(6, test->m_data);
+    CHECK_EQUAL(6, ptr1->m_data);
+
+    Test *test2 = new Test;
+    ptr1 = test2;
+
+    ptr1->m_data = 7;
+
+    CHECK_EQUAL(7, ptr1->m_data);
+
+    //here all data should be deleted
+}
+
+
+TEST(DataPtrTest, ShouldBehaveProperlyForDereferenceOperators)
+{
+    struct Test
+    {
+        Test(): m_data(1) {}
+
+        int m_data;
+    };
+
+    Test *test = new Test;
+    data_ptr<Test> ptr(test);
+
+    POINTERS_EQUAL(test, &(*ptr));
+    POINTERS_EQUAL(&(test->m_data), &(ptr->m_data));
+}
+
+
+TEST(DataPtrTest, ShouldBehaveProperlyForManyAssignOperators)
+{
+    struct Test
+    {
+        Test(): m_data(1) {}
+
+        int m_data;
+    };
+
+    Test *test = new Test;
+    test->m_data = 10;
+
+    data_ptr<Test> ptr1;
+    data_ptr<Test> ptr2;
+    data_ptr<Test> ptr3;
+    data_ptr<Test> ptr4;
+    data_ptr<Test> ptr5;
+
+    ptr1 = ptr2 = ptr3 = ptr4 = ptr5 = test;
+
+    CHECK_EQUAL(10, test->m_data);
+    CHECK_EQUAL(10, ptr1->m_data);
+    CHECK_EQUAL(10, ptr2->m_data);
+    CHECK_EQUAL(10, ptr3->m_data);
+    CHECK_EQUAL(10, ptr4->m_data);
+    CHECK_EQUAL(10, ptr5->m_data);
+
+    ptr1->m_data = 11;
+    ptr2->m_data = 12;
+    ptr3->m_data = 13;
+    ptr4->m_data = 14;
+    ptr5->m_data = 15;
+
+    CHECK_EQUAL(15, test->m_data);       //test == ptr5
+    CHECK_EQUAL(11, ptr1->m_data);
+    CHECK_EQUAL(12, ptr2->m_data);
+    CHECK_EQUAL(13, ptr3->m_data);
+    CHECK_EQUAL(14, ptr4->m_data);
+    CHECK_EQUAL(15, ptr5->m_data);
 }
