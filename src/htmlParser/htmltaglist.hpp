ed88dce1d4c1b64c7534798460e1fde4703f7040
Michał Walenciak <Kicer86@gmail.com>
2013-02-13 20:44:50 +0100

    htmlParser: comments

--- src/htmlParser/htmltaglist.hpp
+++ src/htmlParser/htmltaglist.hpp
@@ -1,5 +1,5 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
+    List of tags which handles theirs level
     Copyright (C) 2011  Michał Walenciak <Kicer86@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
