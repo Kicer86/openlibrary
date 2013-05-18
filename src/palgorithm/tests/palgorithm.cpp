2b61fb77cc12881f67176122c5f1328b333e8466
Michał Walenciak <Kicer86@gmail.com>
2013-05-18 22:16:48 +0200

    palgorithm: test added

--- src/palgorithm/tests/palgorithm.cpp
+++ src/palgorithm/tests/palgorithm.cpp
@@ -22,3 +22,25 @@ TEST(TS_QueueShould, ReturnAllPassedData)
     CHECK_EQUAL(1, i1);
     CHECK_EQUAL(2, i2);
 }
+
+
+TEST(TS_QueueShould, DealWithTakingFromEmptyContainer)
+{
+    TS_Queue<std::deque<int>> queue(10);
+    
+    queue.push_back(1);
+    
+    const int i1 = queue.pop_front();
+    bool throwed = false;
+    
+    try
+    {
+        queue.pop_front(std::chrono::duration<int>::min());
+    }
+    catch(const std::runtime_error &)
+    {
+        throwed = true;
+    }
+    
+    CHECK_EQUAL(true, throwed);    
+}
