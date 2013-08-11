3c4630573d21534156d0094734d4da25fc94f292
Michał Walenciak <Kicer86@gmail.com>
2013-08-11 20:01:14 +0200

    palgorithm: using real count of threads for sort; simplifying main loop

--- src/palgorithm/psort.hpp
+++ src/palgorithm/psort.hpp
@@ -2,13 +2,16 @@
 #ifndef OPENLIBRARY_PALGORITHM_PSORT
 #define OPENLIBRARY_PALGORITHM_PSORT
 
+#include <omp.h>
+
 #include "private/psort_private.hpp"
 
 
 template<class ArrayIterator>
 void sort(ArrayIterator first, ArrayIterator last)
 {
-    OpenLibrary::Private::quick_sort(first, last, 2);
+    const int cores = omp_get_num_threads();
+    OpenLibrary::Private::quick_sort(first, last, cores);
 }
 
 #endif 
