f225a5e11ffdca7a2fbef342cd4c5e36a6486d2a
Michał Walenciak <Kicer86@gmail.com>
2013-08-24 19:50:16 +0200

    palgorithms: psort: speeding up mergesort by tmp buffer switches

--- src/palgorithm/private/psort_private.hpp
+++ src/palgorithm/private/psort_private.hpp
@@ -5,6 +5,7 @@
 #include <assert.h>
 #include <omp.h>
 #include <algorithm>
+#include <type_traits>
 
 namespace OpenLibrary
 {
@@ -278,53 +279,99 @@ namespace OpenLibrary
                 fast_sort(left, right);
         }
         
-
-        void merge_sort(int* array, size_t size)
+        
+        template<class ArrayIterator>
+        struct MergeSortData
         {
+            ArrayIterator main_left;
+            ArrayIterator tmp_left;
+            
+            long size;
+            
+            ArrayIterator getWork(int level)
+            {
+                return level % 2 == 1? main_left : tmp_left;
+            }
+            
+            ArrayIterator getTemp(int level)
+            {
+                return level % 2 == 1? tmp_left : main_left;
+            }
+        };
+        
+        
+        template<class ArrayIterator>
+        void merge_sort_internal(MergeSortData<ArrayIterator> data, int avail_cpus, int level)
+        {
+            const auto size = data.size;
             assert(size > 0);
 
-            if (size == 2)
-            {
-                if (array[0] > array[1])
-                    std::swap(array[0], array[1]);
-            }
-            else if (size > 2)
+            if (size > BoseNelsonSortingNetwork<ArrayIterator>::max_items || level % 2 == 0)
             {
                 const int middle = size / 2;
 
                 const size_t size_1 = middle;
                 const size_t size_2 = size - middle;
-                int* array_1 = array;
-                int* array_2 = array + middle;
 
                 //divide into two parts
+                MergeSortData<ArrayIterator> data1, data2;
+                data1.main_left = data.main_left;
+                data1.tmp_left = data.tmp_left;
+                data1.size = size_1;
+                
+                data2.main_left = data.main_left + middle;
+                data2.tmp_left = data.tmp_left + middle;
+                data2.size = size_2;
 
-                merge_sort(array, middle);
-                merge_sort(array + middle, size - middle);
+                merge_sort_internal(data1, avail_cpus, level + 1);
+                merge_sort_internal(data2, avail_cpus, level + 1);
 
                 //merge them
+                ArrayIterator array_1 = data.getWork(level + 1);           //where level + 1 was working?
+                ArrayIterator array_2 = data.getWork(level + 1) + middle;
                 size_t idx_1 = 0, idx_2 = 0;
-                size_t inserter = 0;
-
-                int* tmp = new int[size];
-
+             
+                ArrayIterator insert = data.getWork(level);                //where do we work?
                 while (idx_1 < size_1 || idx_2 < size_2)
                 {
                     bool use1 = idx_2 == size_2 || (idx_1 < size_1 && array_1[idx_1] < array_2[idx_2]);
 
                     if (use1)
-                        tmp[inserter++] = array_1[idx_1++];
+                    {
+                        *insert = *(array_1 + idx_1);
+                        ++idx_1;
+                    }
                     else
-                        tmp[inserter++] = array_2[idx_2++];
+                    {
+                        *insert = *(array_2 + idx_2);
+                        ++idx_2;
+                    }
+                    
+                    ++insert;
                 }
-
-                for (size_t i = 0; i < size; i++)
-                    array[i] = tmp[i];
-
-                delete [] tmp;
             }
+            else
+                fast_sort(data.getWork(level), data.getWork(level) + data.size);
         }
+        
 
+        template<class ArrayIterator>
+        void merge_sort(ArrayIterator left, ArrayIterator right, int avail_cpus)
+        {
+            typedef typename std::remove_reference<decltype(*left)>::type data_type;
+            const auto size = right - left;
+            auto *tmp = new data_type[size];
+
+            MergeSortData<ArrayIterator> data;
+            data.main_left = left;
+            data.tmp_left = tmp;
+            data.size = size;
+            
+            merge_sort_internal(data, avail_cpus, 1);
+            
+            delete [] tmp;
+        }        
+        
 
         /*****************************************************************************/
 
--- src/palgorithm/psort.hpp
+++ src/palgorithm/psort.hpp
@@ -14,4 +14,11 @@ void sort(ArrayIterator first, ArrayIterator last)
     OpenLibrary::Private::quick_sort(first, last, cores);
 }
 
+template<class ArrayIterator>
+void merge_sort(ArrayIterator first, ArrayIterator last)
+{
+    const int cores = omp_get_num_procs();
+    OpenLibrary::Private::merge_sort(first, last, cores);
+}
+
 #endif 
