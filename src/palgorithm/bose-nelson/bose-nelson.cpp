af6b07093318ba9964788064a3817f42c3e6593f
Michał Walenciak <Kicer86@gmail.com>
2013-08-28 18:38:11 +0200

    palgorithm: bose-nelson: refact

--- src/palgorithm/bose-nelson/bose-nelson.cpp
+++ src/palgorithm/bose-nelson/bose-nelson.cpp
@@ -1,2 +1,80 @@
 
 #include "bose-nelson.hpp"
+
+BoseNelsonSortingNetworkGenerator::BoseNelsonSortingNetworkGenerator(SwapInstructions& v): m_swapInstr(v) 
+{
+    
+}
+
+void BoseNelsonSortingNetworkGenerator::generate(size_t size) const
+{
+    bose(size);
+}
+
+
+void BoseNelsonSortingNetworkGenerator::P(size_t i, size_t j) const
+{
+    //printf("swap(%lu, %lu);\n", i, j);
+    m_swapInstr.push_back(i);
+    m_swapInstr.push_back(j);
+}
+
+
+void BoseNelsonSortingNetworkGenerator::Pbracket(size_t i, size_t x, size_t j, size_t y) const
+
+//int i;  /* value of first element in sequence 1 */
+//int x;  /* length of sequence 1 */
+//int j;  /* value of first element in sequence 2 */
+//int y;  /* length of sequence 2 */
+        
+{
+    if(x == 1 && y == 1)
+        P(i, j); /* 1 comparison sorts 2 items */
+    else if(x == 1 && y == 2)
+    {
+        /* 2 comparisons inserts an item into an
+            * already sorted sequence of length 2. */
+        P(i, (j + 1));
+        P(i, j);
+    }
+    else if(x == 2 && y == 1)
+    {
+        /* As above, but inserting j */
+        P(i, j);
+        P((i + 1), j);
+    }
+    else
+    {
+        /* Recurse on shorter sequences, attempting
+            * to make the length of one subsequence odd
+            * and the length of the other even. If we
+            * can do this, we eventually merge the two. */
+        const size_t a = x / 2;
+        const size_t b = (x & 1) ? (y / 2) : ((y + 1) / 2);
+        Pbracket(i, a, j, b);
+        Pbracket((i + a), (x - a), (j + b), (y - b));
+        Pbracket((i + a), (x - a), j, b);
+    }
+}
+
+
+void BoseNelsonSortingNetworkGenerator::Pstar(size_t i, size_t m) const
+//int i;  /* value of first element in sequence */
+//int m;  /* length of sequence */        
+{
+    if(m > 1)
+    {
+        /* Partition into 2 shorter sequences,
+            * generate a sorting method for each,
+            * and merge the two sub-networks. */
+        const size_t a = m / 2;
+        Pstar(i, a);
+        Pstar((i + a), (m - a));
+        Pbracket(i, a, (i + a), (m - a));
+    }
+}
+void BoseNelsonSortingNetworkGenerator::bose(size_t n) const
+{
+    //Pstar(1, n); /* sort the sequence {X1,...,Xn} */
+    Pstar(0, n); //like in original but start from 0
+}
--- src/palgorithm/bose-nelson/bose-nelson.hpp
+++ src/palgorithm/bose-nelson/bose-nelson.hpp
@@ -14,90 +14,28 @@
 // http://en.wikipedia.org/wiki/Sorting_network
 // parser: sed -e "s@\[\[@\[@g" -e "s@\]\]@\]@g" -e "s@\],\[@\]\[@g" -e "s@\[\([0-9]\+\),\([0-9]\+\)\]@sort_swap<T>(array, \1, \2);\n@g" input
 
-
 class BoseNelsonSortingNetworkGenerator
 {
     public:
         typedef std::vector<int> SwapInstructions;
 
-        constexpr BoseNelsonSortingNetworkGenerator(SwapInstructions &v): m_swapInstr(v) {}
+        BoseNelsonSortingNetworkGenerator(SwapInstructions &v);
 
         //generate sort instructions
-        void generate(size_t size) const
-        {
-            bose(size);
-        }
+        void generate(size_t size) const;
 
     private:
         SwapInstructions &m_swapInstr;
 
-        void P(size_t i, size_t j) const
-        {
-            //printf("swap(%lu, %lu);\n", i, j);
-            m_swapInstr.push_back(i);
-            m_swapInstr.push_back(j);
-        }
-
-        void Pbracket(size_t i, size_t x, size_t j, size_t y) const
-//int i;  /* value of first element in sequence 1 */
-//int x;  /* length of sequence 1 */
-//int j;  /* value of first element in sequence 2 */
-//int y;  /* length of sequence 2 */
-        {
-            if(x == 1 && y == 1)
-                P(i, j); /* 1 comparison sorts 2 items */
-            else if(x == 1 && y == 2)
-            {
-                /* 2 comparisons inserts an item into an
-                    * already sorted sequence of length 2. */
-                P(i, (j + 1));
-                P(i, j);
-            }
-            else if(x == 2 && y == 1)
-            {
-                /* As above, but inserting j */
-                P(i, j);
-                P((i + 1), j);
-            }
-            else
-            {
-                /* Recurse on shorter sequences, attempting
-                    * to make the length of one subsequence odd
-                    * and the length of the other even. If we
-                    * can do this, we eventually merge the two. */
-                const size_t a = x / 2;
-                const size_t b = (x & 1) ? (y / 2) : ((y + 1) / 2);
-                Pbracket(i, a, j, b);
-                Pbracket((i + a), (x - a), (j + b), (y - b));
-                Pbracket((i + a), (x - a), j, b);
-            }
-        }
-
-        void Pstar(size_t i, size_t m) const
-//int i;  /* value of first element in sequence */
-//int m;  /* length of sequence */
-        {
-            if(m > 1)
-            {
-                /* Partition into 2 shorter sequences,
-                    * generate a sorting method for each,
-                    * and merge the two sub-networks. */
-                const size_t a = m / 2;
-                Pstar(i, a);
-                Pstar((i + a), (m - a));
-                Pbracket(i, a, (i + a), (m - a));
-            }
-        }
+        void P(size_t i, size_t j) const;
+        void Pbracket(size_t i, size_t x, size_t j, size_t y) const;
+        void Pstar(size_t i, size_t m) const;
 
         /* Calling bose(n) generates a network
             * to sort n items. See R. C. Bose & R. J. Nelson,
             * "A Sorting Problem", JACM Vol. 9, Pp. 282-296. */
 
-        void bose(size_t n) const
-        {
-            //Pstar(1, n); /* sort the sequence {X1,...,Xn} */
-            Pstar(0, n); //like in original but start from 0
-        }
+        void bose(size_t n) const;
 };
         
 #endif
