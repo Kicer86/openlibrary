0bd7e6cb9e29d2fb4649e1f6fd7ba04350ea89d8
Michał Walenciak <Kicer86@gmail.com>
2013-12-01 13:06:32 +0100

    cmake: adding custom finders for gmock and gtest

---
+++ cmake/FindGMock.cmake
@@ -0,0 +1,32 @@
+
+# useful: http://www.rad.upenn.edu/sbia/software/basis/apidoc/latest/FindGMock_8cmake_source.html
+
+# - Try to find GMock
+# Once done this will define
+#  GMOCK_FOUND - System has LibXml2
+#  GMOCK_INCLUDE_DIRS - The LibXml2 include directories
+#  GMOCK_SOURCES - Extra sources to be added to build
+
+
+find_path(GMOCK_INCLUDE_DIR gmock/gmock.h
+          HINTS ${GMOCK_DIR}/include)
+          
+find_path(GMOCK_INTERNALS src/gmock.cc
+          HINTS ${GMOCK_INCLUDE_DIR}/.. 
+                ${GMOCK_DIR}
+                /usr/src/gmock                 #debian
+         )
+
+find_file(GMOCK_SOURCE  src/gmock.cc
+          HINTS ${GMOCK_INTERNALS})
+
+set(GMOCK_INCLUDE_DIRS ${GMOCK_INCLUDE_DIR} )
+set(GMOCK_SOURCES      ${GMOCK_SOURCE} )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set GMOCK_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(GTest  DEFAULT_MSG
+                                  GMOCK_SOURCE GMOCK_INCLUDE_DIR GMOCK_INTERNALS)
+
+mark_as_advanced(GMOCK_SOURCE GMOCK_INCLUDE_DIR GMOCK_INTERNALS)
---
+++ cmake/FindGTest.cmake
@@ -0,0 +1,30 @@
+
+# - Try to find GTest
+# Once done this will define
+#  GTEST_FOUND - System has LibXml2
+#  GTEST_INCLUDE_DIRS - The LibXml2 include directories
+#  GTEST_SOURCES - Extra sources to be added to build
+
+
+find_path(GTEST_INCLUDE_DIR gtest/gtest.h
+          HINTS ${GTEST_DIR}/include)
+          
+find_path(GTEST_INTERNALS src/gtest.cc
+          HINTS ${GTEST_INCLUDE_DIR}/.. 
+                ${GTEST_DIR}
+                /usr/src/gtest                      #debian
+         )
+
+find_file(GTEST_SOURCE  src/gtest.cc
+          HINTS ${GTEST_INTERNALS})
+          
+set(GTEST_INCLUDE_DIRS ${GTEST_INCLUDE_DIR} ${GTEST_INTERNALS})
+set(GTEST_SOURCES      ${GTEST_SOURCE} )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set GTEST_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(GTest  DEFAULT_MSG
+                                  GTEST_SOURCE GTEST_INCLUDE_DIR GTEST_INTERNALS)
+
+mark_as_advanced(GTEST_SOURCE GTEST_INCLUDE_DIR GTEST_INTERNALS)
