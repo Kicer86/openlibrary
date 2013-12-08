ddb57cfad3a6cf05c25d4b5a3f1e30460853da11
Michał Walenciak <Kicer86@gmail.com>
2013-12-08 10:08:23 +0100

    enableCodeCoverage: main if statement depends now on TRUCOV or LCOV

--- cmake/publicFunctions.cmake
+++ cmake/publicFunctions.cmake
@@ -105,76 +105,80 @@ function(enableCodeCoverage target)
     set(LCOV_DIR         ${CMAKE_BINARY_DIR}/code_coverage/lcov)        #dir for lcov's files
     set(TRUCOV_DIR       ${CMAKE_BINARY_DIR}/code_coverage/trucov)      #dir for trucov's files
    
-    #register lcov's global targets
-    if (LCOV AND NOT TARGET generate_code_coverage)   #register lcov targets only once, for whole binary tree
-               
-        #init counters
-        add_custom_command(OUTPUT ${FLAGS_DIR}/clear
-                           COMMAND lcov --directory ${CMAKE_BINARY_DIR} --zerocounters
-                           COMMAND ${CMAKE_COMMAND} -E make_directory ${FLAGS_DIR}
-                           COMMAND ${CMAKE_COMMAND} -E make_directory ${HTML_OUTPUT_DIR}
-                           COMMAND ${CMAKE_COMMAND} -E make_directory ${LCOV_DIR}
-                           COMMAND ${CMAKE_COMMAND} -E remove -f ${LCOV_DIR}/*
-                           COMMAND ${CMAKE_COMMAND} -E remove -f ${FLAGS_DIR}/*
-                           COMMAND ${CMAKE_COMMAND} -E remove -f ${HTML_OUTPUT_DIR}/index.html  #to force genhtml
-                           COMMAND ${CMAKE_COMMAND} -E touch ${FLAGS_DIR}/clear
-                           WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-                           COMMENT "cleaning code coverage data")
-                
-        #gather info and generate html
-        add_custom_command(OUTPUT ${FLAGS_DIR}/test_run
-                           DEPENDS ${FLAGS_DIR}/clear
-                           DEPENDS ${target}
-                           COMMAND ctest
-                           COMMAND ${CMAKE_COMMAND} -E touch ${FLAGS_DIR}/test_run
-                           WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-                           COMMENT "running tests")
-                           
-        add_custom_target(_cc_prepare                                      #target-related targes will depend on this
-                          DEPENDS ${FLAGS_DIR}/test_run
-                          WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-                          COMMENT "preparing code coverage environment")
+    if(LCOV OR TRUCOV)
 
-        #here is a gap in relations chain filled by "per target build step"
-                          
-        add_custom_target(_cc_gather_data                                  #target-related targets attach here
-                          WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-                          COMMENT "gathering code coverage data")
-                                   
-        add_custom_command(OUTPUT ${HTML_OUTPUT_DIR}/index.html
-                           DEPENDS _cc_gather_data
-                           COMMAND ${GENHTML} --quiet ${LCOV_DIR}/lcov_*.info --output-directory ${HTML_OUTPUT_DIR}
-                           COMMAND ${CMAKE_COMMAND} -E remove -f ${FLAGS_DIR}/clear #not clean anymore
-                           COMMENT "generating html with code coverage information"
-                           WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-                           
-        add_custom_target(generate_code_coverage
-                          DEPENDS ${HTML_OUTPUT_DIR}/index.html
-                          COMMENT "generating html data"
-                          WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-                                                     
-    endif(LCOV AND NOT TARGET generate_code_coverage)
-    
-    #per target build step
-    if(LCOV)
-        get_property(LIB_LOCATION TARGET ${target} PROPERTY LOCATION)
-        get_filename_component(LIB_DIR ${LIB_LOCATION} PATH)
-        add_custom_command(OUTPUT ${LCOV_DIR}/lcov_${target}.info
-                           DEPENDS _cc_prepare
-                           COMMAND ${LCOV} --quiet --capture --directory . --output-file ${LCOV_DIR}/lcov_${target}.info
-                           COMMAND ${LCOV} --quiet --remove ${LCOV_DIR}/lcov_${target}.info '/usr/include/*' '/usr/lib/*' -o ${LCOV_DIR}/lcov_${target}.info
-                           WORKING_DIRECTORY ${LIB_DIR}
-                           COMMENT "gathering code coverage data for target ${target}")
-                           
-        add_custom_target(_lcov_${target}_info_gathering
-                          DEPENDS ${LCOV_DIR}/lcov_${target}.info
-                          WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+        #register global targets
+        if(NOT TARGET generate_code_coverage)   #register lcov targets only once, for whole binary tree
 
-        add_dependencies(_cc_gather_data _lcov_${target}_info_gathering)
-                          
-        get_property(sources TARGET ${target} PROPERTY SOURCES)
-        enableCodeCoverageForSources(${target} ${sources})
-    endif(LCOV)
+            #init counters
+            add_custom_command(OUTPUT ${FLAGS_DIR}/clear
+                               COMMAND lcov --directory ${CMAKE_BINARY_DIR} --zerocounters
+                               COMMAND ${CMAKE_COMMAND} -E make_directory ${FLAGS_DIR}
+                               COMMAND ${CMAKE_COMMAND} -E make_directory ${HTML_OUTPUT_DIR}
+                               COMMAND ${CMAKE_COMMAND} -E make_directory ${LCOV_DIR}
+                               COMMAND ${CMAKE_COMMAND} -E remove -f ${LCOV_DIR}/*
+                               COMMAND ${CMAKE_COMMAND} -E remove -f ${FLAGS_DIR}/*
+                               COMMAND ${CMAKE_COMMAND} -E remove -f ${HTML_OUTPUT_DIR}/index.html  #to force genhtml
+                               COMMAND ${CMAKE_COMMAND} -E touch ${FLAGS_DIR}/clear
+                               WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                               COMMENT "cleaning code coverage data")
+
+            #gather info and generate html
+            add_custom_command(OUTPUT ${FLAGS_DIR}/test_run
+                               DEPENDS ${FLAGS_DIR}/clear
+                               DEPENDS ${target}
+                               COMMAND ctest
+                               COMMAND ${CMAKE_COMMAND} -E touch ${FLAGS_DIR}/test_run
+                               WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                               COMMENT "running tests")
+
+            add_custom_target(_cc_prepare                                      #target-related targes will depend on this
+                              DEPENDS ${FLAGS_DIR}/test_run
+                              WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                              COMMENT "preparing code coverage environment")
+
+            #here is a gap in relations chain filled by "per target build step"
+
+            add_custom_target(_cc_gather_data                                  #target-related targets attach here
+                              WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                              COMMENT "gathering code coverage data")
+
+            add_custom_command(OUTPUT ${HTML_OUTPUT_DIR}/index.html
+                               DEPENDS _cc_gather_data
+                               COMMAND ${GENHTML} --quiet ${LCOV_DIR}/lcov_*.info --output-directory ${HTML_OUTPUT_DIR}
+                               COMMAND ${CMAKE_COMMAND} -E remove -f ${FLAGS_DIR}/clear #not clean anymore
+                               COMMENT "generating html with code coverage information"
+                               WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+
+            add_custom_target(generate_code_coverage
+                              DEPENDS ${HTML_OUTPUT_DIR}/index.html
+                              COMMENT "generating html data"
+                              WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+
+        endif(NOT TARGET generate_code_coverage)
+
+        #per target build step
+        if(LCOV)
+            get_property(LIB_LOCATION TARGET ${target} PROPERTY LOCATION)
+            get_filename_component(LIB_DIR ${LIB_LOCATION} PATH)
+            add_custom_command(OUTPUT ${LCOV_DIR}/lcov_${target}.info
+                               DEPENDS _cc_prepare
+                               COMMAND ${LCOV} --quiet --capture --directory . --output-file ${LCOV_DIR}/lcov_${target}.info
+                               COMMAND ${LCOV} --quiet --remove ${LCOV_DIR}/lcov_${target}.info '/usr/include/*' '/usr/lib/*' -o ${LCOV_DIR}/lcov_${target}.info
+                               WORKING_DIRECTORY ${LIB_DIR}
+                               COMMENT "gathering code coverage data for target ${target}")
+
+            add_custom_target(_lcov_${target}_info_gathering
+                              DEPENDS ${LCOV_DIR}/lcov_${target}.info
+                              WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+
+            add_dependencies(_cc_gather_data _lcov_${target}_info_gathering)
+
+            get_property(sources TARGET ${target} PROPERTY SOURCES)
+            enableCodeCoverageForSources(${target} ${sources})
+        endif(LCOV)
+
+    endif(LCOV OR TRUCOV)
     
 endfunction(enableCodeCoverage)
 
