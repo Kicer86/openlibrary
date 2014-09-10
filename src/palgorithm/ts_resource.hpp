38cbd4a3825ee5ee8cdfe89edca0359d9c8ac1b3
Michał Walenciak <Kicer86@gmail.com>
2014-09-10 22:11:17 +0200

    palgorithm: ThreadSafeResource: code cleanup; fixing issue with operator=() in Accessor

--- src/palgorithm/ts_resource.hpp
+++ src/palgorithm/ts_resource.hpp
@@ -50,56 +50,64 @@ class ThreadSafeResource
     public:
         struct Accessor
         {
-            Accessor(std::mutex& mutex, T& object): m_lock(mutex), m_object(object) {}
-            Accessor(Accessor&& other): m_lock(std::move(other.m_lock)), m_object(other.m_object) {}
+            Accessor(std::mutex& mutex, T* object): m_lock(mutex), m_object(object) {}
+            Accessor(Accessor&& other): m_lock(std::move(other.m_lock)),
+                                        m_object(other.m_object)
+            {
+                other.m_object = nullptr;
+                other.m_notify = nullptr;
+            }
 
             Accessor(const Accessor &) = delete;
             Accessor& operator=(const Accessor &) = delete;
             Accessor& operator=(Accessor&& other)
             {
                 m_lock = std::move(other.m_lock);
+                m_object = other.m_object;
+
+                other.m_object = nullptr;
+                other.m_notify = nullptr;
 
                 return *this;
             }
 
             virtual ~Accessor()
             {
-
             }
 
             T& get()
             {
-                return m_object;
+                return *m_object;
             }
 
             const T& get() const
             {
-                return m_object;
+                return *m_object;
             }
 
             T* operator->()
             {
-                return &m_object;
+                return m_object;
             }
 
             const T* operator->() const
             {
-                return &m_object;
+                return m_object;
             }
 
             T& operator*()
             {
-                return m_object;
+                return *m_object;
             }
 
             const T& operator*() const
             {
-                return m_object;
+                return *m_object;
             }
 
             private:
                 std::unique_lock<std::mutex> m_lock;
-                T& m_object;
+                T* m_object;
         };
 
         friend struct Deleter;
@@ -114,7 +122,7 @@ class ThreadSafeResource
 
         Accessor lock()
         {
-            Accessor accessor(m_mutex, m_resource);
+            Accessor accessor(m_mutex, &m_resource);
 
             return accessor;
         }
