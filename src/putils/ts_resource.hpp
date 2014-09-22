7f0896bbd663746a6f1b431154efcb6fce957a57
Michał Walenciak <Kicer86@gmail.com>
2014-09-22 11:55:08 +0200

    putils: ThreadSafeResource: doxygen comments added

--- src/putils/ts_resource.hpp
+++ src/putils/ts_resource.hpp
@@ -10,9 +10,20 @@
 
 #include "putils_export.h"
 
+
+//! Thread safe wrapper for objects.
+/*!
+   ThreadSafeResource is a template class which allows to wrap any object (resource) into a thread safe container.
+   Resource object must be constructed together with ThreadSafeResource (see ThreadSafeResource::ThreadSafeResource()).
+   Then it can accessed by one thread at once. Any access to resource when it is already locked will cause in wait operation.
+   To access locked resource call ThreadSafeResource::lock(). An Accessor object will be returned which is responsible for resource unlocking 
+   when no longer used.
+ */
+
 template<typename T>
 class ThreadSafeResource
 {
+
         struct Deleter
         {
             Deleter(std::unique_lock<std::mutex>* lock): m_lock(lock) {}
@@ -48,11 +59,18 @@ class ThreadSafeResource
         };
 
     public:
+        //! Notification interface
         struct INotify
         {
-            virtual void unlocked() = 0;
+            virtual void unlocked() = 0;                       //!< Called when Accessor is being destructed and resource is unlocked.
         };
 
+        //! ThreadSafeResource's helper class
+        /*! Accessor is used by ThreadSafeResource for giving exclusive access to resource.
+         *  Resource is normally being kept in ThreadSafeResource object.
+         *  When ThreadSafeResource::lock() is called an Accessor object is returned with exclusive access to resource.
+         *  As long as Accessor is alive, resource cannot be accessed by other threads.
+         */
         struct Accessor
         {
             Accessor(std::mutex& mutex, T* object): m_lock(mutex), m_object(object), m_notify(nullptr) {}
@@ -78,6 +96,11 @@ class ThreadSafeResource
                 return *this;
             }
 
+            //! Destructor
+            /*!
+             * After destruction of Accessor resource is again available to other threads.
+             * If INotify object was used with ThreadSafeResource::lock(INotify *), unlock() will be called on it
+             */
             virtual ~Accessor()
             {
                 if (m_lock)
@@ -87,31 +110,37 @@ class ThreadSafeResource
                     m_notify->unlocked();
             }
 
+            //! get reference to resource
             T& get()
             {
                 return *m_object;
             }
 
+            //! get reference to const resource
             const T& get() const
             {
                 return *m_object;
             }
 
+            //! get pointer to resource
             T* operator->()
             {
                 return m_object;
             }
 
+            //! get pointer to const resource
             const T* operator->() const
             {
                 return m_object;
             }
 
+            //! get reference to resource
             T& operator*()
             {
                 return *m_object;
             }
 
+            //! get reference to const resource
             const T& operator*() const
             {
                 return *m_object;
@@ -131,6 +160,8 @@ class ThreadSafeResource
 
         friend struct Deleter;
 
+        //! Contructor
+        /*! Constructs ThreadSafeResource together with locked resource. */
         template<typename... Args>
         ThreadSafeResource(const Args&... args): m_mutex(), m_resource(args...)
         {
@@ -139,6 +170,8 @@ class ThreadSafeResource
         ThreadSafeResource(const ThreadSafeResource<T> &) = delete;
         ThreadSafeResource<T>& operator=(const ThreadSafeResource<T> &) = delete;
 
+        //! Locks a resource and returns access to it.
+        //! As long as returned Accessor object exists the resource will be locked and unavailable for other threads.
         Accessor lock()
         {
             Accessor accessor(m_mutex, &m_resource);
@@ -146,7 +179,8 @@ class ThreadSafeResource
             return accessor;
         }
 
-        //behaves as lock() but sends notification when object is being unlocked
+        //! Locks a resource and returns access to it.
+        //! Behaves as lock() but calls INotify::unlocked() when object is being unlocked.
         Accessor lock(INotify* notify)
         {
             Accessor accessor = lock();
