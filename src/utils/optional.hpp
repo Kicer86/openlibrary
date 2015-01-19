418c00614ceaa258c45bed25b80a90ffef03f1bc
Michał Walenciak <Kicer86@gmail.com>
2015-01-19 18:41:57 +0100

    utils: placing optional and data_ptr into ol namespace

--- src/utils/optional.hpp
+++ src/utils/optional.hpp
@@ -20,77 +20,82 @@
 #ifndef OPENLIBRARY_UTILS_OPTIONAL_HPP
 #define OPENLIBRARY_UTILS_OPTIONAL_HPP
 
-template<typename T>
-class Optional
+namespace ol
 {
-    public:
-        Optional(): m_data(), m_present(false) {}
-        Optional(const Optional<T>& other): m_data(other.m_data), m_present(other.m_present) {}
-        Optional(const T& data): m_data(data), m_present(true) {}
-        Optional(Optional<T>&& other): m_data(std::move(other.m_data)), m_present(other.m_present) {}
 
-        Optional<T>& operator=(const Optional<T>& other)
-        {
-            m_data = other.m_data;
-            m_present = other.m_present;
+    template<typename T>
+    class Optional
+    {
+        public:
+            Optional(): m_data(), m_present(false) {}
+            Optional(const Optional<T>& other): m_data(other.m_data), m_present(other.m_present) {}
+            Optional(const T& data): m_data(data), m_present(true) {}
+            Optional(Optional<T>&& other): m_data(std::move(other.m_data)), m_present(other.m_present) {}
 
-            return *this;
-        }
+            Optional<T>& operator=(const Optional<T>& other)
+            {
+                m_data = other.m_data;
+                m_present = other.m_present;
 
-        Optional<T>& operator=(const T& data)
-        {
-            m_data = data;
-            m_present = true;
+                return *this;
+            }
 
-            return *this;
-        }
+            Optional<T>& operator=(const T& data)
+            {
+                m_data = data;
+                m_present = true;
 
-        Optional<T>& operator=(T&& data)
-        {
-            m_data = std::move(data);
-            m_present = true;
+                return *this;
+            }
 
-            return *this;
-        }
+            Optional<T>& operator=(T&& data)
+            {
+                m_data = std::move(data);
+                m_present = true;
 
-        const T* operator->() const
-        {
-            return &m_data;
-        }
+                return *this;
+            }
 
-        T* operator->()
-        {
-            return &m_data;
-        }
+            const T* operator->() const
+            {
+                return &m_data;
+            }
 
-        const T& operator*() const
-        {
-            return m_data;
-        }
+            T* operator->()
+            {
+                return &m_data;
+            }
 
-        T& operator*()
-        {
-            return m_data;
-        }
+            const T& operator*() const
+            {
+                return m_data;
+            }
 
-        bool operator!() const
-        {
-            return !m_present;
-        }
+            T& operator*()
+            {
+                return m_data;
+            }
 
-        operator bool() const
-        {
-            return m_present;
-        }
+            bool operator!() const
+            {
+                return !m_present;
+            }
 
-        bool is_initialized() const
-        {
-            return m_present;
-        }
+            operator bool() const
+            {
+                return m_present;
+            }
 
-    private:
-        T m_data;
-        bool m_present;
-};
+            bool is_initialized() const
+            {
+                return m_present;
+            }
+
+        private:
+            T m_data;
+            bool m_present;
+    };
+
+}
 
 #endif // OPENLIBRARY_UTILS_OPTIONAL_HPP
