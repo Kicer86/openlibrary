ce0fd618e69224541f769c9c99719b328c69aab2
Michał Walenciak <Kicer86@gmail.com>
2015-01-26 22:01:11 +0100

    CMake: copying Finder for GTest and GMock from broom

--- cmake/FindGMock.cmake
+++ cmake/FindGMock.cmake
@@ -3,30 +3,53 @@
 
 # - Try to find GMock
 # Once done this will define
-#  GMOCK_FOUND - System has GMock
+#  GMOCK_FOUND        - System has GMock
 #  GMOCK_INCLUDE_DIRS - The GMock include directories
-#  GMOCK_SOURCES - Extra sources to be added to build
-
+#  GMOCK_LIBRARY      - GMock library
+#  GMOCK_MAIN_LIBRARY - GMock main library
 
 find_path(GMOCK_INCLUDE_DIR gmock/gmock.h
           HINTS ${GMOCK_DIR}/include)
-          
-find_path(GMOCK_INTERNALS src/gmock-all.cc
-          HINTS ${GMOCK_INCLUDE_DIR}/.. 
-                ${GMOCK_DIR}
-                /usr/src/gmock                 #debian
-         )
 
-find_file(GMOCK_SOURCE  src/gmock-all.cc
-          HINTS ${GMOCK_INTERNALS})
+find_library(GMOCK_LIBRARY      libgmock.a)
+find_library(GMOCK_MAIN_LIBRARY libgmock_main.a)
 
-set(GMOCK_INCLUDE_DIRS ${GMOCK_INCLUDE_DIR} ${GMOCK_INTERNALS})
-set(GMOCK_SOURCES      ${GMOCK_SOURCE} )
+set(GMOCK_INCLUDE_DIRS ${GMOCK_INCLUDE_DIR})
+
+if(GMOCK_INCLUDE_DIR AND NOT GMOCK_LIBRARY)
+
+    if(NOT TARGET gmock)
+
+        message("Trying to find GMock sources and register extra targets")
+        find_file(GMOCK_BASE_SOURCE src/gmock-all.cc
+                  HINTS /usr/src/gmock ${GMOCK_DIR})
+
+        find_file(GMOCK_MAIN_SOURCE src/gmock_main.cc
+                  HINTS /usr/src/gmock ${GMOCK_DIR})
+
+        if(NOT GMOCK_BASE_SOURCE OR NOT GMOCK_MAIN_SOURCE)
+            message(FATAL_ERROR "Could not find base for GMock sources. Set GMOCK_DIR to proper value")
+        endif(NOT GMOCK_BASE_SOURCE OR NOT GMOCK_MAIN_SOURCE)
+
+        add_library(gmock STATIC ${GMOCK_BASE_SOURCE})
+        add_library(gmock-main STATIC ${GMOCK_MAIN_SOURCE})
+
+        get_filename_component(gmock_base_dir ${GMOCK_BASE_SOURCE} PATH)
+        set_target_properties(gmock gmock-main PROPERTIES INCLUDE_DIRECTORIES ${gmock_base_dir}/../)
+
+        #there should be a CMakeLists.txt for GMock however it introduces a GTest targets also which may conflict with targets from FindGTest
+
+    endif(NOT TARGET gmock)
+
+    set(GMOCK_LIBRARY gmock gtest)
+    set(GMOCK_MAIN_LIBRARY gmock-main)
+
+endif(GMOCK_INCLUDE_DIR AND NOT GMOCK_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set GMOCK_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(GTest  DEFAULT_MSG
-                                  GMOCK_SOURCE GMOCK_INCLUDE_DIR GMOCK_INTERNALS)
+find_package_handle_standard_args(GMock DEFAULT_MSG
+                                  GMOCK_INCLUDE_DIR GMOCK_BASE_SOURCE GMOCK_MAIN_SOURCE)
 
-mark_as_advanced(GMOCK_SOURCE GMOCK_INCLUDE_DIR GMOCK_INTERNALS)
+mark_as_advanced(GMOCK_INCLUDE_DIR GMOCK_BASE_SOURCE GMOCK_MAIN_SOURCE)
