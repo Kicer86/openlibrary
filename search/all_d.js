c219011fd7b18f7d9d10aa21d31e494368aab13a
Michał Walenciak <Kicer86@gmail.com>
2015-07-11 21:40:00 +0200

    updating documentation

--- search/all_d.js
+++ search/all_d.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['waitfordata',['waitForData',['../d2/d36/classol_1_1_t_s___queue.html#aaa5c6110aa024f1e034f0dbf6162612e',1,'ol::TS_Queue']]]
+  ['wait_5ffor_5fdata',['wait_for_data',['../d2/d36/classol_1_1_t_s___queue.html#af589d6326c0fe024cae643c3f80160b9',1,'ol::TS_Queue']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['pop_5ffront',['pop_front',['../d2/d36/classol_1_1_t_s___queue.html#a11eab37d87f9b83f5a8c59c42b29fed1',1,'ol::TS_Queue']]],
-  ['push_5fback',['push_back',['../d2/d36/classol_1_1_t_s___queue.html#a39419363454c3bdbf2b7ecd69cce77e8',1,'ol::TS_Queue::push_back(const T &amp;item)'],['../d2/d36/classol_1_1_t_s___queue.html#a982c2dd46e1b22aa921972b8cd71913c',1,'ol::TS_Queue::push_back(T &amp;&amp;item)']]]
+  ['pop',['pop',['../d2/d36/classol_1_1_t_s___queue.html#a4162a90ed1ad7456a2dfb1f9765a4399',1,'ol::TS_Queue']]],
+  ['pop_5ffor',['pop_for',['../d2/d36/classol_1_1_t_s___queue.html#a8e0295a9cb19de5ac0ce638530620fa6',1,'ol::TS_Queue']]],
+  ['push',['push',['../d2/d36/classol_1_1_t_s___queue.html#aa938934d5d6302372454104214113d64',1,'ol::TS_Queue::push(const T &amp;item)'],['../d2/d36/classol_1_1_t_s___queue.html#a007b9487e1a019b625c2963c9739c89d',1,'ol::TS_Queue::push(T &amp;&amp;item)']]]
 ];
