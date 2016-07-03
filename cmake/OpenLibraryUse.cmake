ec2c9ba409960c3af6f13a118c706e6754cb346a
Michal Walenciak <Kicer86@gmail.com>
2016-07-03 21:47:22 +0200

    cmake: do not hide symbols for static libraries
    It causes static libraries to be empty

--- cmake/OpenLibraryUse.cmake
+++ cmake/OpenLibraryUse.cmake
@@ -64,9 +64,15 @@ function(hideSymbols target)
 
     if(TARGET ${target})
 
-        set_target_properties(${target} PROPERTIES CXX_VISIBILITY_PRESET hidden
-                                                   VISIBILITY_INLINES_HIDDEN 1)
-
+		get_target_property(target_type ${target} TYPE)
+	
+		if(NOT target_type STREQUAL "STATIC_LIBRARY")
+		
+			set_target_properties(${target} PROPERTIES CXX_VISIBILITY_PRESET hidden
+													   VISIBILITY_INLINES_HIDDEN 1)
+													   
+		endif()
+	  
     endif(TARGET ${target})
 
 endfunction(hideSymbols)
