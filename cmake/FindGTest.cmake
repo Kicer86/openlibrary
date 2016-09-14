5242a70a46e7a98d846947b0fd2f0cb9a5629b73
Michał Walenciak <Kicer86@gmail.com>
2016-09-14 22:09:21 +0200

    cmake: syncing gmock and gtest finders with photo_broom

--- cmake/FindGTest.cmake
+++ cmake/FindGTest.cmake
@@ -9,7 +9,8 @@
 #  GTEST_MAIN_LIBRARY - GTest main library
 
 find_path(GTEST_INCLUDE_DIR gtest/gtest.h
-          HINTS ${GTEST_DIR}/include)
+          HINTS ${GTEST_DIR}/include
+                ${GMOCK_DIR}/gtest/include)
 
 find_library(GTEST_LIBRARY      libgtest.a)
 find_library(GTEST_MAIN_LIBRARY libgtest_main.a)
@@ -22,10 +23,16 @@ if(GTEST_INCLUDE_DIR AND NOT GTEST_LIBRARY)
 
         message("Trying to find GTest sources and register extra targets")
         find_file(GTEST_BASE_SOURCE src/gtest-all.cc
-                  HINTS /usr/src/gtest ${GTEST_DIR})
+                  HINTS /usr/src/gtest
+                        ${GTEST_DIR}
+                        ${GMOCK_DIR}/gtest
+                  )
 
         find_file(GTEST_MAIN_SOURCE src/gtest_main.cc
-                  HINTS /usr/src/gtest ${GTEST_DIR})
+                  HINTS /usr/src/gtest
+                        ${GTEST_DIR}
+                        ${GMOCK_DIR}/gtest
+                  )
 
         if(NOT GTEST_BASE_SOURCE OR NOT GTEST_MAIN_SOURCE)
             message(FATAL_ERROR "Could not find base for GTest sources. Set GTEST_DIR to proper value")
@@ -35,8 +42,18 @@ if(GTEST_INCLUDE_DIR AND NOT GTEST_LIBRARY)
         add_library(gtest-main STATIC ${GTEST_MAIN_SOURCE})
 
         get_filename_component(gtest_base_dir ${GTEST_BASE_SOURCE} PATH)
-        set_target_properties(gtest gtest-main PROPERTIES INCLUDE_DIRECTORIES ${gtest_base_dir}/../)
+        target_include_directories(gtest      
+                                    PRIVATE 
+                                        ${gtest_base_dir}/..
+                                        ${gtest_base_dir}/../include 
+        )
 
+        target_include_directories(gtest-main 
+                                    PRIVATE 
+                                        ${gtest_base_dir}/..
+                                        ${gtest_base_dir}/../include 
+        )
+                        
     endif(NOT TARGET gtest)
 
     set(GTEST_LIBRARY gtest)
