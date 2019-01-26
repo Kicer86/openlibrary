26ea708acdd25e7e1cd7506a3aedaad473ab717c
Michał Walenciak <Kicer86@gmail.com>
2019-01-26 10:07:30 +0100

    adjusting names

--- conanfile.py
+++ conanfile.py
@@ -15,18 +15,18 @@ class OpenlibraryConan(ConanFile):
 
     def source(self):
         self.run("git clone https://github.com/Kicer86/openlibrary.git")
-        self.run("cd hello && git checkout static_shared")
+        self.run("cd openlibrary && git checkout master")
         # This small hack might be useful to guarantee proper /MT /MD linkage
         # in MSVC if the packaged project doesn't have variables to set it
         # properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)",
-                              '''PROJECT(MyHello)
+        tools.replace_in_file("openlibrary/CMakeLists.txt", "project(openlibrary)",
+                              '''project(openlibrary)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="hello")
+        cmake.configure(source_folder="openlibrary")
         cmake.build()
 
         # Explicit way:
@@ -35,13 +35,13 @@ conan_basic_setup()''')
         # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="openlibrary")
+        self.copy("*openlibrary.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["openlibrary"]
 
