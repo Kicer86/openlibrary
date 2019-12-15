28bc48c9186b028717a4584192241ec9a1eb7d29
Michał Walenciak <Kicer86@gmail.com>
2019-12-15 21:59:02 +0100

    preparations for test

--- tests/utils_qt/configuration_tests.cpp
+++ tests/utils_qt/configuration_tests.cpp
@@ -1,1 +1,12 @@
 
+#include <gmock/gmock.h>
+
+#include "OpenLibrary/utils_qt/configuration.hpp"
+#include "iconfiguration_storage_mock.hpp"
+
+
+TEST(Configuration, isEmptyAfterConstruction)
+{
+    IConfigStorageMock storage;
+    Configuration config(storage);
+}
